b91257ab7ad07e51e6497d191fa8157ff1c4e495
f0ng <422342338@qq.com>
2022-05-15 18:58:42 +0800

    Update codeEncode.java

--- src/com/autoDecoder/util/codeEncode.java
+++ src/com/autoDecoder/util/codeEncode.java
@@ -15,8 +15,13 @@ import sun.misc.BASE64Encoder;
 public class codeEncode {
     public static void main(String[] args) throws Exception {
 
-        String a = encryptKeyivmode("{\"id\":\"1''\"}","f0ngtest","f0ngf0ng","DES","CBC","PKCS5Padding","Base64","无");
+        String a = encryptKeyivmode("{\"id\":\"1''\"}",
+                "f0ngtestf0ngtest","f0ngf0ng","DESede","CBC","PKCS5Padding","Base64","无");
         System.out.println(a);
+
+        String aa = encryptKeyivmode("{\"id\":\"1''\"}",
+                "f0ngtestf0ngtest","f0ngf0ng","DESede","CBC","PKCS7Padding","Base64","无");
+        System.out.println(aa);
     }
 
     public static String byte2hex(final byte[] array) {
@@ -46,13 +51,38 @@ public class codeEncode {
         return array3;
     }
 
+    public static String encrypt2(byte[] array) {
+        try {
+            byte[] array2 = toByteArray(new String(array));
 
+            String key_str = "f7dd8981859e6b2932c72eddf39aec144dd43896ca95252f30293188fd033abc";
+            String iv_str = "dfb04ad776cc1ff90ea26e441a3949db";
+            byte[] iv = toByteArray(iv_str);
+            byte[] key = toByteArray(key_str);
+            final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            final SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+            final Cipher instance = Cipher.getInstance("AES/CFB/NoPadding");
+            instance.init(1, secretKeySpec, ivParameterSpec);
+            String array3 = byte2hex(byteMerger(instance.getIV(), instance.doFinal(array)));
+            return array3;
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 
     public static String encryptKeyivmode(String encryptedData,String sessionKey,String iv,String encodemode, String ivmode , String paddingmode ,String sSrcmode ,String keyivmode) throws Exception {
         try {
             byte[] data = null;
             byte[] aseKey ;
             byte[] ivData ;
+
+            if (encodemode.equals("DESede")){
+                if (sessionKey.length() > 24){
+                    sessionKey = sessionKey.substring(0,24);
+                }
+            }
 
             data = encryptedData.getBytes(StandardCharsets.UTF_8);
             if (keyivmode.equals("Base64")) {
@@ -65,14 +95,21 @@ public class codeEncode {
                 aseKey = sessionKey.getBytes(StandardCharsets.UTF_8);
                 ivData = iv.getBytes(StandardCharsets.UTF_8);
             }
+
             IvParameterSpec ivParameterSpec = new IvParameterSpec(ivData);
             Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
             Cipher cipher = Cipher.getInstance(encodemode + "/" + ivmode + "/" + paddingmode);
             Key sKeySpec = new SecretKeySpec(aseKey, encodemode);
-            cipher.init(Cipher.ENCRYPT_MODE, sKeySpec,ivParameterSpec );// 初始化
+            //cipher.init(Cipher.ENCRYPT_MODE, sKeySpec,ivParameterSpec );// 初始化
+            if (ivmode.equals("ECB") || ivmode.equals("GCM")) { // 如果为ECB模式，不进行偏移量加载
+                cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
+            } else {
+                cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, ivParameterSpec);
+            }
+
             byte[] result = cipher.doFinal(data);
             String final_result = "";
-            if (sSrcmode.equals("Base64") || sSrcmode.equals("无"))
+            if (sSrcmode.equals("Base64") || sSrcmode.equals("null"))
                 final_result = new String(Base64.getEncoder().encode(result)).replace("\n","");
 
             if (sSrcmode.equals("Hex"))
